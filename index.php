9af3a3cb3f59ed74f535a68f864f6bcca920d1c7
rico <liberiamor@gmail.com>
2021-07-16 22:15:42 +0300

    form procedure

--- index.php
+++ index.php
@@ -6,48 +6,28 @@
 <body>
     <!--<script src="/sources/main.js"></script>-->
 	<?php
-		
-		class Entree {
-			private $name;
-			protected $ingridients;
-
-			public function __construct($name, $ingridients) {
-				if (! is_array) {
-					throw new Exception('$ingridients must be an array');
-				}
-				$this->name = $name;
-				$this->ingridients = $ingridients;
-			}
-
-			public function hasIngridient($ingridient) {
-				return in_array($ingridient, $this->ingridients);
-			}
-		}
-
-		class Ingridient {
-			public function __construct($name, $price) {
-				$this->name = $name;
-				$this->price = $price;
-			}
-
-			public function change_price($price) {
-				$this->price = $price;
-			}
-
-			public function info() {
-				print "$this->name - \$$this->price<br>\n";
-			}
-
-		}
-
-		class Meal extends Ingridient {
-			public function __construct($name, $ingridients)
-
-		$tomate = new Ingridient("Tomato", 3.45);
-		$tomate->info();
-		$tomate->change_price(5.45);
-		$tomate->info();
-
+        
+        print <<<HTML
+        <form action="{$_SERVER['PHP_SELF']}" method="post">
+            <select name="lunch[]" multiple>
+                <option value="pork">Pork</option>
+                <option value="beef">Beef</option>
+                <option value="fish">Fish</option>
+            </select>
+            <button>Send</button>
+        </form>
+HTML;
+        
+        if (count($_POST) > 0) {
+            print "<p>Submitted values:</p>";
+            foreach ($_POST['lunch'] as $meal) {
+                print "<li>$meal</li>";
+            }
+        }
+            
+    print_r($_POST);
 	?>
+    
+
 </body>
 </html>
