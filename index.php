5a7bb9ba93f2c7891cfce5b5713b637cef0f0820
ricotioloco <liberiamor@gmail.com>
2021-08-03 20:11:05 +0300

    string exercise

--- index.php
+++ index.php
@@ -2,28 +2,40 @@
     require_once "sources/html_tpl.php";
     head_html("Home page");
     
+    function myprint_r ($obj) {
+        print "<pre>";
+        print_r($obj);
+        print "</pre>";
+    }
+
     function myecho (...$arguments) {
         foreach ($arguments as $argument) {
             echo "$argument\n<br>";
         }
     }
 
-    myecho(...[1,2,3,4,5,'sting']);
+    //myecho(...[1,2,3,4,5,'sting']);
 
-    function counter () {
+    /*function counter () {
         static $count = 0;
         $count++;
         print $count;
+    }*/
+    
+    function tabber ($spaces, ...$args) {
+        $arr = [];
+        /*foreach ($args as $values) {
+            $arr[] = str_repeat("&nbsp;", $spaces).$values;
+        }*/
+        for ($i = 0; $i < count($args); $i++) {
+            $arr[] = str_repeat("&nbsp;", $spaces + $i).$args[$i];
+        }
+        myecho (...$arr);
     }
+    tabber (10, "string1", "string2", "string3");
 
 
-    for ($i = 0; $i < 5; $i++) {
-        counter();
-    }
-
-    print "<br>\n";
-    //print "<pre>";
-    print_r($GLOBALS);
-    //print "</pre>";
     end_html();
+
+
 ?>
